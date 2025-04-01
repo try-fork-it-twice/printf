5d03e7fb9de15332a412ebf249a0867e27160138
Alexander Nalobin <m_troc@mail.ru>
2025-04-02 01:54:35 +0300

    Add generator for example-test traces

---
+++ tests/scripts/example_bin_file_gen.py
@@ -0,0 +1,68 @@
+import struct
+from icecream import ic
+
+TASK_CREATE = 0
+TASK_SWITCHED_IN = 1
+TASK_SWITCHED_OUT = 2
+
+
+class TraceGenerator:
+    def __init__(self):
+        self.buffer = bytearray()
+
+    def task_create(self, timestamp: int, task_number: int, task_name: str):
+        fmt = "<B I I 64s"
+        encoded = task_name.encode("utf-8")
+        if len(encoded) >= 64:
+            encoded = encoded[:63] + b"\0"
+        else:
+            encoded = encoded + b"\0" + b"\0" * (64 - len(encoded) - 1)
+        self.buffer.extend(
+            struct.pack(fmt, TASK_CREATE, timestamp, task_number, encoded)
+        )
+        return self
+
+    def task_switched_in(self, timestamp: int, task_number: int):
+        fmt = "<B I I"
+        self.buffer.extend(struct.pack(fmt, TASK_SWITCHED_IN, timestamp, task_number))
+        return self
+
+    def task_switched_out(self, timestamp: int, task_number: int):
+        fmt = "<B I I"
+        self.buffer.extend(struct.pack(fmt, TASK_SWITCHED_OUT, timestamp, task_number))
+        return self
+
+    def save(self, filename: str):
+        with open(filename, "w+b") as f:
+            f.write(self.buffer)
+        ic("Saved trace file to", filename)
+        return self
+
+
+def gen() -> TraceGenerator:
+    return TraceGenerator()
+
+
+if __name__ == "__main__":
+    traces_dir = "./tests/traces/"
+    gen().task_create(
+        timestamp=1000, task_number=1, task_name="TaskAlpha"
+    ).task_switched_in(timestamp=1500, task_number=1).task_switched_out(
+        timestamp=2000, task_number=1
+    ).save(
+        f"{traces_dir}trace1.bin"
+    )
+
+    (
+        gen()
+        .task_create(timestamp=0, task_number=1, task_name="init")
+        .task_switched_in(timestamp=110, task_number=1)
+        .task_switched_out(timestamp=120, task_number=1)
+        .task_create(timestamp=130, task_number=2, task_name="file_open")
+        .task_create(timestamp=135, task_number=3, task_name="file_read")
+        .task_switched_in(timestamp=140, task_number=2)
+        .task_switched_in(timestamp=145, task_number=3)
+        .save(f"{traces_dir}trace2.bin")
+    )
+
+    gen().task_switched_in(timestamp=10, task_number=1).save(f"{traces_dir}trace3.bin")
